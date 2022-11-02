05289cc8491dba3503843d73b7290293a1b8556a
Destiny <miguelycar@gmail.com>
2022-11-02 08:48:44 +0100

    #2

--- app/src/main/java/com/example/e1mdmelomigueleduardo/VtnInicio.kt
+++ app/src/main/java/com/example/e1mdmelomigueleduardo/VtnInicio.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.*
 import androidx.navigation.fragment.findNavController
-import com.example.e1mdmelomigueleduardo.databinding.FragmentFirstBinding
+import androidx.viewbinding.ViewBindings
+import com.example.e1mdmelomigueleduardo.databinding.InicioxmlBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class VtnInicio : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: InicioxmlBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,17 +26,19 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var nombreusu = ((R.id.usuariotxt) as EditText)
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = InicioxmlBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+// Si pulsas continuar abre el segundo panel
         binding.buttonContinuar.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
         }
     }
 
--- app/src/main/java/com/example/e1mdmelomigueleduardo/VtnOpciones.kt
+++ app/src/main/java/com/example/e1mdmelomigueleduardo/VtnOpciones.kt
@@ -7,38 +7,48 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.e1mdmelomigueleduardo.databinding.FragmentSecondBinding
+import com.example.e1mdmelomigueleduardo.databinding.FragmentBienvenidoBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class VtnOpciones : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private var _binding1: FragmentBienvenidoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val binding1 get() = _binding1!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
+        _binding1 = FragmentBienvenidoBinding.inflate(inflater,container, false)
+        return binding1.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+        binding.volver.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        binding.logEnter.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_bienvenido)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        _binding1 = null
     }
 }
